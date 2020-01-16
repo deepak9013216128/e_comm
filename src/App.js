5381a538621f9b1e4b3c6e3583cb3dbc783d810b
deepak/9013216128 <deepak9013216128@gmail.com>
2020-01-16 18:17:28 +0530

    add header component to homepage,shoppage and style it

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Switch ,Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShowPage from './pages/shop/shop.component'
+import Header from './component/header/header.component';
 
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShowPage} />
