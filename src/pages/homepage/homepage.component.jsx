18f8959e9d7da8a451340df10ac8d13a039e82bb
deepak/9013216128 <deepak9013216128@gmail.com>
2020-01-16 10:46:24 +0530

    create homepage component and style the menu-item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './homepage.style.scss';
+import Directory from '../../component/directory/directory.component';
+
+const HomePage = () =>(
+    <Directory />
+);
+export default HomePage;
+     
