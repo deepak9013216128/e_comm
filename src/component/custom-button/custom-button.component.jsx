d08ca5b60b34b3c80a42592e65577aa258d3ff9b
deepak/9013216128 <deepak9013216128@gmail.com>
2020-02-01 18:17:39 +0530

    added to favicon icon , title and fix the google sign in button color

--- src/component/custom-button/custom-button.component.jsx
+++ src/component/custom-button/custom-button.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './custom-button.style.scss';
 
 const CustomButton = ({children,isGoogleSignIn,inverted,...otherProps}) =>(
-    <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sgin-in' : ''}  custom-button`} {...otherProps}>
+    <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''}  custom-button`} {...otherProps}>
         {children}
     </button>
 );
