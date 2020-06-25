8406855f1430a4c2e6db867803f4bb1137e550b3
deepak9013216128 <deepak9013216128@gmail.com>
2020-06-25 12:24:15 +0530

    sperate the cra into client folder and create server for accpet stripe payment

--- client/src/component/stripe-button/stripe-button.component.jsx
+++ client/src/component/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
-
+import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({price})=>{
-    const stripePrice = price * 100;
+    const priceForStripe = price * 100;
     const publishableKey = 'pk_test_DmjsQvd5gqEWE106gstJltFq00qaKcEGxd';
 
     const onToken = token =>{
-        console.log(token);
-        alert('Payment Successful');
+        axios({
+            url: '/payment',
+            method: 'post',
+            data: {
+              amount: priceForStripe,
+              token: token
+            }
+          })
+        .then(response => {
+            alert('succesful payment');
+        })
+        .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+            'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+        });
     }
 
     return (
@@ -19,7 +34,7 @@ const StripeCheckoutButton = ({price})=>{
             shippingAddress
             image = 'http://sendeyo.com/up/d/f3eb2117da'
             description = {`Your total is $${price}`}
-            amount = {stripePrice}
+            amount = {priceForStripe}
             panelLabel = 'Pay Now'
             token={onToken}
             stripeKey = {publishableKey}
